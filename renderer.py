15062d9243ccabffc6c5cc197a6de3f254eba41c
dabao-ziwei <stephenwu.0926@gmail.com>
2025-12-24 06:52:07 +0800

    Update renderer.py

--- renderer.py
+++ renderer.py
@@ -1,4 +1,3 @@
-# renderer.py
 from logic import GAN, ZHI
 
 def get_palace_html(idx, branch, r, c, info, is_daxian, is_liunian):
@@ -7,7 +6,8 @@ def get_palace_html(idx, branch, r, c, info, is_daxian, is_liunian):
     if is_liunian: classes.append("active-liunian")
     class_str = " ".join(classes)
     
-    # 1. 主星區塊
+    # 1. 主星區塊 (Main Stars)
+    # 每個主星是一個獨立的 .star-major-container，裡面垂直排列：名字 -> 四化
     main_stars_html = ""
     for star in info['major_stars']:
         sihua_html = ""
@@ -15,10 +15,9 @@ def get_palace_html(idx, branch, r, c, info, is_daxian, is_liunian):
             bg_cls = {"本":"bg-ben", "大":"bg-da", "流":"bg-liu"}.get(sh['layer'], "")
             sihua_html += f"<span class='hua-badge {bg_cls}'>{sh['type']}</span>"
         
-        # 關鍵修正：單行拼接
         main_stars_html += f"<div class='star-major-container'><div class='star-name'>{star['name']}</div>{sihua_html}</div>"
     
-    # 2. 副星區塊
+    # 2. 副星區塊 (Minor Stars)
     sub_stars_html = ""
     for m_name, is_bad, is_impt in info['minor_stars']:
         style_cls = ""
@@ -34,6 +33,7 @@ def get_palace_html(idx, branch, r, c, info, is_daxian, is_liunian):
     if is_daxian: status_tags += "<div class='tag-flow tag-da'>大限</div>"
     
     # 4. 底部資訊
+    # 這裡將 ganzhi-label 與 zhi-label 放在 footer-left
     footer_html = (
         f"<div class='cell-footer'>"
         f"<div class='footer-left'><span class='ganzhi-label'>{GAN[info['gan_idx']]}</span><span class='zhi-label'>{branch}</span></div>"
@@ -41,11 +41,12 @@ def get_palace_html(idx, branch, r, c, info, is_daxian, is_liunian):
         f"</div>"
     )
 
-    # 5. 組合最終 HTML (單行)
+    # 5. 組合最終 HTML
+    # 注意：main_stars_html 直接放在 stars-box 裡，不再包一層 main-stars-col，這樣多顆主星可以自然並排
     final_html = (
         f"<div class='{class_str}' style='grid-row: {r}; grid-column: {c};'>"
         f"<div class='stars-box'>"
-        f"<div class='main-stars-col'>{main_stars_html}</div>"
+        f"{main_stars_html}"  
         f"<div class='sub-stars-col'>{sub_stars_html}</div>"
         f"</div>"
         f"{footer_html}"
@@ -55,7 +56,6 @@ def get_palace_html(idx, branch, r, c, info, is_daxian, is_liunian):
     return final_html
 
 def get_center_html(data, calc_obj):
-    # 中宮資訊
     return (
         f"<div class='center-info-box'>"
         f"<h3 style='margin:0;color:#000;font-size:24px;'>{data['name']}</h3>"
