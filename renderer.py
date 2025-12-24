0670e8a946d86cff0b191d05a4cb09a824b7e94a
dabao-ziwei <stephenwu.0926@gmail.com>
2025-12-24 14:21:59 +0800

    Update renderer.py

--- renderer.py
+++ renderer.py
@@ -67,16 +67,21 @@ def render_full_chart_html(calc, data, d_idx, l_off, focus_idx):
             stars += f"<div class='star-item'><span class='{cls_s}'>{m[0]}</span></div>"
             
         ft_l = f"<div class='footer-left'><div class='god-box'><span class='god-text' style='color:#008080'>{info['sui_12'][0]}</span><span class='god-text' style='color:#4682B4'>{info['jiang_12'][0]}</span></div><div class='limit-txt'>{info['age_start']}-{info['age_end']}</div></div>"
-        
         p_name = get_relative_palace_name(calc.ming_pos, idx)
         p_html = f"<div class='palace-txt'>{p_name}</div>"
         if not is_pure:
             if l_pos != -1: p_html = f"<div style='color:#0056b3;font-size:11px;font-weight:900'>流{get_relative_palace_name(l_pos, idx)[0]}</div>" + p_html
             if d_pos != -1: p_html = f"<div style='color:#666;font-size:11px;font-weight:900'>大{get_relative_palace_name(d_pos, idx)[0]}</div>" + p_html
-            
         ft_r = f"<div class='footer-right'><div class='info-box'>{'<span class=shen-tag>身</span>' if is_pure and idx==calc.shen_pos else ''}<span style='font-size:11px;color:#800080;font-weight:bold'>{info['life_stage']}</span>{p_html}</div><div class='ganzhi-txt'>{GAN[info['gan_idx']]}{ZHI[idx]}</div></div>"
         
-        cells_html += f"<div id='p_{idx}' class='zwds-cell {border_cls}' style='grid-row:{r};grid-column:{c};'><div class='cell-content {bg_cls}'><div class='stars-box'>{stars}</div>{ft_l}{ft_r}</div></div>"
+        # 關鍵修正：style="pointer-events: none !important;" 寫死在 cell-content 上
+        cells_html += f"""
+        <div id='p_{idx}' class='zwds-cell {border_cls}' style='grid-row:{r};grid-column:{c};'>
+            <div class='cell-content {bg_cls}' style='pointer-events: none !important;'>
+                <div class='stars-box'>{stars}</div>{ft_l}{ft_r}
+            </div>
+        </div>
+        """
 
     center_html = f"<div class='center-cell'><h3 style='margin:0;color:#000'>{data['name']}</h3><div style='font-size:12px;color:#666'>{data['gender']} | {calc.bureau_name} | {data.get('ming_star','')}坐命</div><div style='font-size:12px;font-weight:bold;color:#2E7D32'>國：{data['y']}/{data['m']}/{data['d']} {data['h']}:{data['min']:02d}</div><div style='font-size:12px;color:#555'>農：{calc.lunar.getYearInGanZhi()}年 {calc.lunar.getMonthInChinese()}月 {calc.lunar.getDayInChinese()}</div></div>"
 
@@ -85,7 +90,8 @@ def render_full_chart_html(calc, data, d_idx, l_off, focus_idx):
     for i in range(12):
         inf = limits[i][1]
         cls = "time-btn btn-on" if i == d_idx else "time-btn"
-        d_html += f"<div id='d_{i}' class='{cls}'>{lnames[i]}<br>{GAN[inf['gan_idx']]}{ZHI[inf['zhi_idx']]}</div>"
+        # 關鍵：按鈕內部文字也不攔截點擊
+        d_html += f"<div id='d_{i}' class='{cls}'><div style='pointer-events: none !important;'>{lnames[i]}<br>{GAN[inf['gan_idx']]}{ZHI[inf['zhi_idx']]}</div></div>"
     
     l_row = ""
     if not is_pure:
@@ -96,7 +102,7 @@ def render_full_chart_html(calc, data, d_idx, l_off, focus_idx):
             yr = data['y'] + age - 1
             gy, zy = get_ganzhi_for_year(yr)
             cls = "time-btn btn-on" if j == l_off else "time-btn"
-            l_html += f"<div id='l_{j}' class='{cls}'>{yr}<br>{GAN[gy]}{ZHI[zy]}({age})</div>"
+            l_html += f"<div id='l_{j}' class='{cls}'><div style='pointer-events: none !important;'>{yr}<br>{GAN[gy]}{ZHI[zy]}({age})</div></div>"
         l_row = f"<div class='timeline-container' style='grid-template-columns: repeat(10, 1fr); border-top:none;'>{l_html}</div>"
 
     full_html = f"""{get_css()}<div class="master-container">{clean(svg_html)}<div class="zwds-grid">{clean(cells_html)}{clean(center_html)}</div><div class="timeline-container">{clean(d_html)}</div>{clean(l_row)}</div>"""
